c7d2677cf9208c8cd4e96b1a9ba51453d0de5669
Anuj Kumar Pandit <anujpandit299@gmail.com>
2024-07-05 20:33:08 +0530

    Added new project folders

---
+++ Ecommerce landing page/script.js
@@ -0,0 +1,15 @@
+
+
+// Remove show class from all content items
+function removeShow() {
+	tabContentItems.forEach(item => {
+		item.classList.remove('show');
+	});
+}
+
+const shopNowBtn = document.getElementById('shop-now-btn');
+const productsSection = document.getElementById('products-section');
+
+shopNowBtn.addEventListener('click', () => {
+    productsSection.classList.toggle('show');
+});
